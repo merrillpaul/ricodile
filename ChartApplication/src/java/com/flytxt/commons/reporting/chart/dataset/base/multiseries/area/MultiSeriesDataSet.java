c6d0c64359b76b7b142a1d7579913314130a19a1
san.vishnu <san.vishnu@03646d06-356a-3447-10f7-4dc6f26c61c5>
2010-06-01 10:37:28 +0000

    MultiSeries Area chart Data Set configuration classes

---
+++ ChartApplication/src/java/com/flytxt/commons/reporting/chart/dataset/base/multiseries/area/MultiSeriesDataSet.java
@@ -0,0 +1,25 @@
+package com.flytxt.commons.reporting.chart.dataset.base.multiseries.area;
+
+import com.flytxt.commons.reporting.chart.dataset.base.Set; 
+/**
+ * 
+ * @author vishnu.sankar
+ */
+public class MultiSeriesDataSet extends Set {
+
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public MultiSeriesDataSet () {
+    }
+
+}
+
