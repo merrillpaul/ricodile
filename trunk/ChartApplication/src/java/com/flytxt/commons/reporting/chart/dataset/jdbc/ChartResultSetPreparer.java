9dfd50766f8f4f40dd7dae8a28ff11fdfec7a5b1
san.vishnu <san.vishnu@03646d06-356a-3447-10f7-4dc6f26c61c5>
2010-06-01 10:45:31 +0000

    MultiSeries colum bar chart, modified the getMethod which retruns the Data set for the multiseries/single series chart with and with out dril down parameters

--- trunk/ChartApplication/src/java/com/flytxt/commons/reporting/chart/dataset/jdbc/ChartResultSetPreparer.java
+++ trunk/ChartApplication/src/java/com/flytxt/commons/reporting/chart/dataset/jdbc/ChartResultSetPreparer.java
@@ -14,20 +14,25 @@ package com.flytxt.commons.reporting.chart.dataset.jdbc;
 
 import com.flytxt.commons.reporting.chart.ChartContext;
 import com.flytxt.commons.reporting.chart.dataset.creators.JDBCResultsetAwareCreator;
+import com.flytxt.commons.reporting.chart.dataset.creators.JDBCResultsetAwareCreator.ResultSetColumnInfo;
+import com.flytxt.commons.reporting.chart.dataset.creators.JDBCResultsetAwareCreator.SeriesColumnNameMapping;
 import com.flytxt.commons.reporting.chart.entity.ChartPromptParameters;
 import com.flytxt.commons.reporting.connection.ConnectionProvider;
+import com.flytxt.commons.reporting.constants.ChartConstants.ChartResultSetConstants;
 import com.flytxt.commons.reporting.parameter.ParameterProviderException;
 import com.flytxt.commons.reporting.parameter.objects.Parameter;
 import com.flytxt.commons.reporting.util.query.QueryReportPreparedStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import net.sf.jasperreports.engine.JRException;
 
 /**
  *
@@ -107,6 +112,142 @@ public class ChartResultSetPreparer {
         return new QueryReportPreparedStatement(query, ctx, con).getStatement(filledParameters);
         
     }
+
+    public ResultSetColumnInfo getColumnInfo(ResultSetMetaData rsmds) throws SQLException {
+
+       int columnCount = rsmds.getColumnCount();
+        List<String> columnNames =  new ArrayList<String>(columnCount);
+        List<String> drillDownNames =  new ArrayList<String>();
+       for(int i =0;i<columnCount;i++){
+           columnNames.add(rsmds.getColumnName(i+1).toUpperCase());
+       }
+
+        List<SeriesColumnNameMapping> mapping = null;
+
+       /*  1. select label "category name", value from sometable
+     *  In this case this is a single series chart. The series name would be null*/
+       if(columnCount<3){
+
+            mapping =  new ArrayList<SeriesColumnNameMapping>();
+            SeriesColumnNameMapping eachMap =  new SeriesColumnNameMapping();
+            eachMap.seriesColumnName=null;
+            eachMap.valueColumnName= columnNames.get(1);
+            mapping.add(eachMap);
+        }else if(columnCount==3){
+
+            /*  */
+            if(columnNames.contains(ChartResultSetConstants.SERIESNAME.name())){
+                 mapping =  new ArrayList<SeriesColumnNameMapping>();
+                SeriesColumnNameMapping eachMap =  new SeriesColumnNameMapping();
+                eachMap.seriesColumnName=ChartResultSetConstants.SERIESNAME.name();
+                eachMap.valueColumnName= columnNames.get(1);
+                mapping.add(eachMap);
+            }else{
+                mapping =  new ArrayList<SeriesColumnNameMapping>();
+                String thirdColumnName = columnNames.get(2);
+                SeriesColumnNameMapping eachMap =  new SeriesColumnNameMapping();
+                if(thirdColumnName.startsWith(ChartResultSetConstants.DRILLDOWN.name())){
+                    eachMap.seriesColumnName=null;
+                    eachMap.valueColumnName= columnNames.get(1);
+                    drillDownNames.add(thirdColumnName);
+
+                }else{
+                      eachMap.seriesColumnName = columnNames.get(2);
+                      eachMap.valueColumnName = columnNames.get(1);
+                }
+                mapping.add(eachMap);
+
+              
+
+                // TODO drilldown handling
+            }
+        }else{
+
+                 mapping =  new ArrayList<SeriesColumnNameMapping>();
+                
+                 boolean seriesFound =  false;
+                 boolean drilldownFound = false;
+                 for(String columnName :columnNames){
+                     if(columnName.startsWith(ChartResultSetConstants.DRILLDOWN.name())){
+                        drillDownNames.add(columnName);
+                        drilldownFound=true;
+                     }
+                     if(seriesFound==false){
+                        if(columnName.startsWith(ChartResultSetConstants.SERIESNAME.name())){
+                             seriesFound = true;
+                        }
+                     }
+                 }
+                // check whether there is series
+                // if series not there then the same as adding just a single series
+                // column with probably some drilldown coluklm parameters
+                 if(!seriesFound){
+                        if(drilldownFound){ // asuming that all the rest of the columns are drilldowns
+                            for(int i=2;i<columnNames.size();i++){
+                                  if(!drillDownNames.contains(columnNames.get(i))){
+                                      drillDownNames.add(columnNames.get(i));
+                                  }
+                            }
+                        }
+                        
+                          SeriesColumnNameMapping eachMap =  new SeriesColumnNameMapping();
+                          eachMap.seriesColumnName = null;
+                          eachMap.valueColumnName = columnNames.get(1);
+                          mapping.add(eachMap);
+
+                 }else{
+                         List<String> seriesColumLabels = new ArrayList<String>();
+                         List<String> seriesValueColumLabels = new ArrayList<String>();
+                          for(String colName :columnNames){
+                            if(colName.startsWith(ChartResultSetConstants.SERIESNAME.name())){
+                                seriesColumLabels.add(colName);
+                            }
+                             if(colName.startsWith(ChartResultSetConstants.SERIESVALUE.name())){
+                                seriesValueColumLabels.add(colName);
+                            }
+                         }
+                         for(String seriesName :seriesColumLabels){
+                             String seriesSuffix = (
+                                     seriesName.length()==ChartResultSetConstants.SERIESNAME.name().length()?
+                                         "":seriesName.substring(ChartResultSetConstants.SERIESNAME.name().length())
+                                         );
+
+                            String seriesValueColName = ChartResultSetConstants.SERIESVALUE.name()+seriesSuffix;
+                            SeriesColumnNameMapping eachMap =  new SeriesColumnNameMapping();
+                            eachMap.seriesColumnName = seriesName;
+                            eachMap.valueColumnName = columnNames.contains(seriesValueColName)?seriesValueColName:null;
+                            mapping.add(eachMap);
+
+                         }
+
+
+                           for(int i=2;i<columnNames.size();i++){
+                                  if(!drillDownNames.contains(columnNames.get(i)) && !seriesColumLabels.contains(columnNames.get(i)) && !seriesValueColumLabels.contains(columnNames.get(i))){
+                                      drillDownNames.add(columnNames.get(i));
+                                  }
+                            }
+
+                 }
+
+
+               // if there are series coluns , the n find out which all series coluns a
+               // are there and add to the seriescolumn maping list
+               // for the moment forget about drilldowns
+
+          
+
+
+        }
+
+
+
+
+
+       
+       return new ResultSetColumnInfo(mapping,drillDownNames/* TODO add drilldown coluns here*/);
+    }
+
+
 
 
    
